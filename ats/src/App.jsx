dfe10af210e8c72325470818827e5d046e672573
thenraja01 <thenwthen@gmail.com>
2025-10-06 23:52:51 +0530

    first commit

---
+++ ats/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Nav from './assets/Nav'
+import Uploader from './assets/uploader'
+import Result from './assets/Result'
+import Suggest from './assets/Suggest'
+import Layouts from './Layout/Layouts'
+
+function App() {
+
+  return (
+    <>
+    <Layouts />
+     
+    </>
+  )
+}
+
+export default App
---
+++ ats/src/Layout/Layouts.jsx
@@ -0,0 +1,16 @@
+import Nav from "../assets/Nav"
+import Result from "../assets/Result"
+import Uploader from "../assets/uploader"
+export default function Layouts() {
+    return(<div className="">
+
+        {/* <Nav /> */}
+        <div className="">
+            <Uploader />
+        <Result/>
+        {/* <Outlet /> */}
+        </div>
+    </div>
+    )
+    
+};
